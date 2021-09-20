b47a520cc755d62320e08f08a4d656e392905f80
Niklas Eicker <git@nikl.me>
2021-09-20 22:15:12 +0200

    Start subscriptions endpoint (3.8.3)

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,23 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             .route("/", web::get().to(greet))
             .route("/health-check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
             .default_service(web::to(HttpResponse::NotFound))
     })
     .listen(listener)?
     .run();
 
     Ok(server)
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 async fn greet(req: HttpRequest) -> impl Responder {
