8ce68dada7116db2ec5f7f48d6071cbe8572262c
Niklas Eicker <git@nikl.me>
2021-09-27 22:18:01 +0200

    Start logging (4.4)

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 
@@ -12,8 +13,9 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route(
-                "/health-check",
+                "/health_check",
                 web::get().to(crate::routes::health_check::health_check),
             )
             .route(
